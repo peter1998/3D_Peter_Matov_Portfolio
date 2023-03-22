50a111d0efd71f325a85f80bd5040d0faf6005ef
Peter Matov <pmatov@gmail.com>
2023-03-22 20:03:08 +0200

    [C] another golden hammer fix on the 3D model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,22 +5,18 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = () => {
-  // const computer = useGLTF("./desktop_pc/scene.gltf");
   const computer = useGLTF("./desktop_pc/scene.gltf");
-
+  console.log(computer);
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
-      />
       <pointLight intensity={1} />
-      <primitive object={computer.scene} rotation={[-0.01, -0.2, -0.1]} />
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, 1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   );
 };
@@ -28,16 +24,19 @@ const Computers = () => {
 const ComputersCanvas = () => {
   return (
     <Canvas
-      frameLoop="demand"
+      frameloop="demand"
       shadows
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      camera={{
+        position: [20, 3, 5],
+        fov: 25,
+      }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
+          enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
-          enableZoom={false}
         />
         <Computers />
       </Suspense>
@@ -45,5 +44,4 @@ const ComputersCanvas = () => {
     </Canvas>
   );
 };
-
-export default Computers;
+export default ComputersCanvas;
